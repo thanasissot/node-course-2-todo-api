7961a3fecb4c8a15bc435427567cb61b95a0ee93
thanasissot <thanasissot1@gmail.com>
2018-07-20 16:37:39 +0300

    setup up for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ const url = 'mongodb://127.0.0.1:27017/TodoApp';
 // Tell mongoose to use the build in Promise(JS vanilla)
 mongoose.Promise = global.Promise;
 // connect to the mongo database
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || url, { useNewUrlParser: true });
 // create a mongoose model
 
 module.exports = {mongoose};
